38384ec7d7edead1ce439410128593e00200ee15
mahery <ramanamiantramahery@gmail.com>
2026-09-09 13:17:12 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Mpikajy")
+
+isa_1 = int(input("Ampidiro ny isa voalohany: "))   
+isa_2 = int(input("Ampidiro ny isa faharoa: "))
+
+print("Ny vokatry ny fanampiana dia:", isa_1 + isa_2)
+
+
