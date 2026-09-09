44f8e1a922806a2551b2b3bdb8b0d43f3a4191e3
mahery <ramanamiantramahery@gmail.com>
2026-09-09 13:32:26 +0300

    Fanambarana aminy master anle Asa main

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 print("Mpikajy")
 
+print("Misafidy ny asa tianao atao:")
+print("1. Fanampiana")
+print("2. Fanalana")
+
+choix = int(input("Mifanafatra ny asa tianao (1 na 2): "))
+
 isa_1 = int(input("Ampidiro ny isa voalohany: "))   
 isa_2 = int(input("Ampidiro ny isa faharoa: "))
 
-print("Ny vokatry ny fanampiana dia:", isa_1 + isa_2)
+if choix == 1:
+    print("Ny vokatry ny fanampiana dia:", isa_1 + isa_2)
+elif choix == 2:
+    print("Ny vokatry ny fanalana dia:", fanalana(isa_1, isa_2))
 
 
