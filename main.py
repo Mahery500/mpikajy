31f97d122c941c68b76b8b9d8774f0dcaac0197f
mahery <ramanamiantramahery@gmail.com>
2026-09-09 13:55:01 +0300

    Fanambarana ny fampitomboana aminy ambony

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ print("Mpikajy")
 print("Misafidy ny asa tianao atao:")
 print("1. Fanampiana")
 print("2. Fanalana")
+print("3. Fampitomboana")
 
-choix = int(input("Mifanafatra ny asa tianao (1 na 2): "))
+choix = int(input("Mifanafatra ny asa tianao (1, 2 na 3): "))
 
 isa_1 = int(input("Ampidiro ny isa voalohany: "))   
 isa_2 = int(input("Ampidiro ny isa faharoa: "))
@@ -13,5 +14,6 @@ if choix == 1:
     print("Ny vokatry ny fanampiana dia:", isa_1 + isa_2)
 elif choix == 2:
     print("Ny vokatry ny fanalana dia:", fanalana(isa_1, isa_2))
-
+elif choix == 3:    
+    print("Ny vokatry ny fampitomboana dia:", fampitomboana(isa_1, isa_2))    
 
